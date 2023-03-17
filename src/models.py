0a92aeb9f0ba1bf74b001980616c70e35f039a0d
Ivan Gerasimov <ivan.gerasimov@mdinc.ru>
2023-03-17 18:49:26 +0300

    changes: fixed things for training

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
+import segmentation_models_pytorch as smp
 import torch.nn as nn
 import torch.nn.functional as F
 import yaml
@@ -66,10 +67,42 @@ class WrappedSegformerForSemanticSegmentation(SegformerForSemanticSegmentation):
         return super().forward(input)
 
 
+class WrappedUnetPlusPlus(smp.UnetPlusPlus):
+    def to(self, device):
+        super().to(device)
+        self.loss.to(device)
+
+    def add_loss_function(self, loss: nn.Module) -> None:
+        self.loss = loss
+
+    def set_up_type_upsample(self, resource: Union[Path, str, Dict[str, Any]]):
+        """
+        if downsample_target == True,
+        then target will be downsamplet to size of model
+        and loss will be computed with smallest tensors
+        and vise wersa
+        """
+        self.sampler = UpDownSampler(resource)
+
+    def forward(self, input: Tensor, target: Tensor) -> Dict[str, Tensor]:
+        logits = self.predict(input)
+        logits, target = self.sampler(input=logits, target=target)
+        assert (
+            logits.shape[-2:] == target.shape[-2:]
+        ), "Something wrong in UpDownSampler"
+
+        loss_result = self.loss(input=logits, target=target)
+        return dict(logits=logits, loss=loss_result)
+
+    def predict(slef, input: Tensor) -> Tensor:
+        label = super().forward(input)
+        return label
+
+
 def loadSegformerForSemanticSegmentation(
     resource: Union[Path, str, Dict[str, Any]],
     pre_trained_name: str = "nvidia/mit-b5",
-):
+) -> nn.Module:
 
     loss = load_loss(resource)
 
@@ -84,8 +117,27 @@ def loadSegformerForSemanticSegmentation(
     return model
 
 
+def loadUnetPlusPlus(
+    resource: Union[Path, str, Dict[str, Any]],
+    pre_trained_name: str = "imagenet",
+) -> nn.Module:
+    loss = load_loss(resource)
+
+    model = WrappedUnetPlusPlus(
+        encoder_name="resnet34",
+        encoder_weights=pre_trained_name,
+        in_channels=3,
+        classes=2,
+    )
+
+    model.add_loss_function(loss)
+    model.set_up_type_upsample(resource)
+    return model
+
+
 models_load_functions = {
-    "SegformerForSemanticSegmentation": loadSegformerForSemanticSegmentation
+    "SegformerForSemanticSegmentation": loadSegformerForSemanticSegmentation,
+    "UnetPlusPlus": loadUnetPlusPlus,
 }
 
 
