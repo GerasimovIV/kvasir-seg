521b3e88a9b27390e328cf468e4ebde9ed330dcf
Ivan Gerasimov <ivan.gerasimov@mdinc.ru>
2023-03-19 00:55:27 +0300

    changes: added scripts for testing

--- test.py
+++ test.py
@@ -11,7 +11,9 @@ from src.models import load_model, load_weights_from_checkpoint
 from train import CustomTrainer
 
 
-def test_model(resource: Union[str, Path, Dict[str, Any]]) -> Dict[str, float]:
+def test_model(
+    resource: Union[str, Path, Dict[str, Any]], return_pred: bool = False
+) -> Dict[str, float]:
     test_config = resource
     if isinstance(resource, str) or isinstance(resource, Path):
         resource = Path(resource)
@@ -20,25 +22,29 @@ def test_model(resource: Union[str, Path, Dict[str, Any]]) -> Dict[str, float]:
 
     model = load_model(test_config)
     path_to_checkpoint = test_config["checkpoint"]
+    device = test_config["device"]
     load_weights_from_checkpoint(path_to_checkpoint, model)
     compute_metrics = ComputeMetrics(test_config)
     test_dataset = load_dataset(test_config)
 
+    model.to(device)
     model.eval()
     test_preds = []
     test_labels = []
     with torch.no_grad():
         for data in tqdm(test_dataset, desc="getting predictions"):
-            input = data["input"].unsqueeze(0)
-            target = data["target"].unsqueeze(0)
+            input = data["input"].unsqueeze(0).to(device)
+            target = data["target"].unsqueeze(0).to(device)
 
             logits = model.predict(input)
-            test_preds.append(logits)
-            test_labels.append(target)
+            test_preds.append(logits.to("cpu"))
+            test_labels.append(target.to("cpu"))
 
     test_preds = torch.cat(test_preds, dim=0)
     test_labels = torch.cat(test_labels, dim=0)
 
     metrics = compute_metrics([test_preds, test_labels])
 
+    if return_pred:
+        return metrics, test_preds, test_labels
     return metrics
